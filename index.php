912fee9fcf9393a75e5e393c6d4da447ad334367
chanothit <chanothit37372@gmail.com>
2023-01-30 10:06:58 +0700

    M

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html>
 
-<body>
+<head>
+    <title>Personal Information</title>
+</head>
 
+<body>
+    <h1>Personal Information</h1>
     <form action="show.php">
-        what is your name:<input type="text" name="name"><br>
-        what is your age:<input type="text"name="age"><br>
-        what is your 
-        <input type="submit" value="Submit">
+        what is your name :<input type="text" name="name"><br>
+        what is your age :<input type="text" name="age"><br>
+        what is your sex (male/female):<input type="text" name="sex"><br>
+        what is your marry_status (single/married):<input type="text" name="status"><br>
+        <input type="Submit" value="submit">
     </form>
 </body>
 
--- show.php
+++ show.php
@@ -1,14 +1,56 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <title>Personal Information</title>
+</head>
 
 <body>
+<h1>Personal Information</h1>
     <?php
+    
     $name = $_GET["name"];
     $age = $_GET["age"];
+    $sex = $_GET["sex"];
+    $status = $_GET["status"];
 
-    echo "Name:".$name."<br>";
-    echo "Age:".$age."<br>";
+    echo "Name : ";
+    if ($age <= "14") 
+    { if ($sex == "male") 
+        {
+            echo "Mstr."; 
+        }
+        else if ($sex == "female")
+        {
+           echo "Miss"; 
+        }
+    } 
+    else if ($age >= "15") 
+    { if ($sex == "male")
+        {
+            echo "Mr."; 
+        }
+        else if ($sex == "female")
+        { if ($status == "married") 
+            {
+                echo "Mrs."; 
+            }
+            else if ($status == "single") 
+            {
+                echo "Miss"; 
+            }
+            
+        }
+    }
+    
+    
+    echo "  " . "$name" . "<br>";
+    echo "Age : " . "$age" . "<br>";
+    echo "Sex : " . "$sex" . "<br>";
+    echo "Status : " . "$status" . "<br>";
+
     ?>
+
+
 </body>
 
 </html>
