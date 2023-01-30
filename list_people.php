b56523009e5b97673c1110a142baf002b7f789a8
chanothit <chanothit37372@gmail.com>
2023-01-30 10:42:31 +0700

    ?

--- list_people.php
+++ list_people.php
@@ -10,5 +10,18 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+
+$sql = "SELECT id, fname, lname FROM survey";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["fname"]. " " . $row["lname"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
 ?>
