5050af813c0f1f3a79345c48bbc5c337ab7f08e4
chanothit <chanothit37372@gmail.com>
2023-01-30 10:44:50 +0700

    M

--- list_people.php
+++ list_people.php
@@ -2,15 +2,15 @@
 $servername = "localhost";
 $username = "cpe0696";
 $password = "Ham882545";
+$db_name = "cpe0696";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $db_name);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
 
 $sql = "SELECT id, fname, lname FROM survey";
 $result = $conn->query($sql);
