4962461aebc4543d4f44420ca747afa21d00bee6
juthamatkhamman <juthamat.kh@ku.th>
2023-02-13 10:53:23 +0700

    Add

--- save_edit_people.php
+++ save_edit_people.php
@@ -9,7 +9,8 @@ $sex=$_POST["sex"];
 $marry_status=$_POST["marry_status"];
 
 //echo "fname:$fname lname:$lname age:$age sex:$sex marry_status:$marry_status";
-$sql="UPDATE survey set fname='$fname',lname='$lname',age='$age',sex='$sex',marry_status='$marry_status' WHERE id=$id";
+$sql="INSERT INTO survey(fname,lname,age,sex,marry_status)";
+$sql.=" VALUES('$fname ','$lname ','$age ','$sex ','$marry_status')";
 $conn->query($sql);
 header( "location: list_people.php" );
 ?>
